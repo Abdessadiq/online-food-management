b8c6fd7cf3a2a3b390e0e9837c94e5e6d1a7bd79
Imrane ABDALLAH <imrane.abdallah@byos.ma>
2025-05-20 19:49:05 +0200

    fffff

--- orders/static/orders/main.js
+++ orders/static/orders/main.js
@@ -65,7 +65,6 @@ function check_user_super(){
 }
 
 function add_to_cart(info){
-  incrementCartCount();
   //info will be the stuff displayed in the reciept
   // item description as well as teh price
   display_notif("add to cart", info);
